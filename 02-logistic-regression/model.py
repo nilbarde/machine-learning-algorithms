bd199f61552b56429c94a03c233af6f9e2e8e45a
nilbarde <nihalbarde@gmail.com>
2024-02-17 13:20:18 +0000

    logistic regression working

---
+++ 02-logistic-regression/model.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append("..")
+
+import numpy as np
+
+from common import ModelBase
+
+
+def sigmoid(x):
+    return 1 / (1 + np.e ** (-x))
+
+class LogisticRegression(ModelBase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def initWeights(self):
+        self.weights = np.zeros((self.data_handler.numFeatures() + 1))
+
+    def optimizeWeights(self, train_x, train_y, pred_y, learning_rate):
+        error_y = pred_y - train_y
+        self.weights -= learning_rate * ((error_y.dot(train_x)) / train_x.shape[0])
+
+    def getPredictionMaths(self, data):
+        return sigmoid((data * self.weights).sum(axis=1))
